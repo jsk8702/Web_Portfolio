3c962f7b2a99f2393568050b6138e170df25843f
jsk8702 <47409469+jsk8702@users.noreply.github.com>
2019-12-09 10:07:59 +0900

    2019.12.09
    네비게이터, 메인 사진 수정

--- js/script01.js
+++ js/script01.js
@@ -1,17 +1,8 @@
 (function($){
     
-    //페럴렉스 효과
-    var nav = ("#nav ul li");
-    var cont = ("#contents > div")
-
-    nav.click(function(e){
+    $("#header .header #Mnav").click(function(e){
         e.preventDefault();
-        var taget = $(this);
-        var index = taget.index();
-        var section = cont.eq(index);
-        var offset = section.offset().top;
-        $("html, body").animate({scrollTop: offset},600,"easeInOutExpo");
-    
+        $(".header > ul > li").slideToggle();
     });
 })(jQuery);
 
