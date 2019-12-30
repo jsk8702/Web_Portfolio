ec199fa63dad600b610b73ce0553a2823901ce0c
jsk8702 <47409469+jsk8702@users.noreply.github.com>
2019-12-30 14:17:02 +0900

    2019.12.30

---
+++ js/script06.js
@@ -0,0 +1,21 @@
+(function($){
+    
+    //배너 슬라이드
+//    var mySwiper = new Swiper('.swiper-container', {
+//        //슬라이드 양쪽에 화살표 
+//        navigation: {
+//            nextEl: '.swiper-button-next',
+//            prevEl: '.swiper-button-prev',
+//        },
+//        // 슬라이드 하단에 원형 표시
+//        pagination: {
+//            el: '.swiper-pagination',
+//            type: 'bullets',
+//        },
+//        autoplay: {
+//            delay: 3000,
+//        },
+//    });
+    
+})(jQuery);
+
